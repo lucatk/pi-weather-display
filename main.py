d68bc2f318ea0f2a2521d997ac5fda6c5de75d37
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-23 01:55:59 +0200

    Anzeigemodi

--- main.py
+++ main.py
@@ -14,12 +14,44 @@ import time
 from datetime import datetime
 
 class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
+
+    mode_temp = 0
+    mode_clock = 0
+
+    def changeTempMode(self):
+        if self.mode_temp > 2:
+            self.mode_temp = 0
+        else:
+            self.mode_temp = self.mode_temp+1
+
+    def changeMCPMode(self):
+        if self.mode_temp == 0:
+            self.mode_temp = 1
+        elif self.mode_temp == 1:
+            self.mode_temp = 0
+        elif self.mode_temp == 2:
+            self.mode_temp = 3
+        elif self.mode_temp == 3:
+            self.mode_temp = 2
+
+    def changeClockMode(self):
+        if self.mode_temp == 0:
+            self.mode_temp = 1
+        else:
+            self.mode_temp = 0
+
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.showFullScreen()
+
+        self.lblTemperature.clicked.connect(lambda: self.changeTempMode())
+        self.lblHumidity.clicked.connect(lambda: self.changeMCPMode())
+        self.lblLightLevel.clicked.connect(lambda: self.changeMCPMode())
+        self.lblSmokeLevel.clicked.connect(lambda: self.changeMCPMode())
+        self.lblTime.clicked.connect(lambda: self.changeTimeMode())
 
         bgPalette = self.centralWidget.palette()
         bgPalette.setColor(self.centralWidget.backgroundRole(), QtCore.Qt.black)
@@ -61,13 +93,26 @@ def receive(form):
         for val in datastore["dht_temperature"]:
             sum += val
         mittel = float(sum) / len(datastore["dht_temperature"])
-        form.lblTemperature.setText(str(round(mittel, 1)).replace(".", ",") + "﻿°C")
+        if form.mode_temp == 0:
+            form.lblTemperature.setText(str(round(mittel, 1)).replace(".", ",") + "﻿°C")
+        elif form.mode_temp == 1:
+            form.lblTemperature.setText(str(round(mittel)) + "﻿°C")
+        elif form.mode_temp == 2:
+            mittel = (mittel * (9/5)) + 32
+            form.lblTemperature.setText(str(round(mittel, 1)) + "﻿°F")
+        elif form.mode_temp == 3:
+            mittel = (mittel * (9/5)) + 32
+            form.lblTemperature.setText(str(round(mittel)) + "﻿°F")
+
 
         sum = 0
         for val in datastore["dht_humidity"]:
             sum += val
         mittel = float(sum) / len(datastore["dht_humidity"])
-        form.lblHumidity.setText("Feuchtigkeit: " + str(round(mittel, 1)).replace(".", ",") + "%")
+        if form.mode_temp % 2 == 0:
+            form.lblHumidity.setText("Feuchtigkeit: " + str(round(mittel, 1)).replace(".", ",") + "%")
+        else:
+            form.lblHumidity.setText("Feuchtigkeit: " + str(round(mittel)) + "%")
 
         sum = 0
         for val in datastore["mcp_light_voltage"]:
@@ -76,16 +121,25 @@ def receive(form):
         lightLevel = ((1-(float(mittel)/1023))/0.6)*100
         if lightLevel > 100:
             lightLevel = 100
-        form.lblLightLevel.setText("Es ist " + str(round(lightLevel, 1)) + "% hell.")
+        if form.mode_temp % 2 == 0:
+            form.lblLightLevel.setText("Es ist " + str(round(lightLevel, 1)).replace(".", ",") + "% hell.")
+        else:
+            form.lblLightLevel.setText("Es ist " + str(round(lightLevel)) + "% hell.")
 
         sum = 0
         for val in datastore["mcp_smoke_voltage"]:
             sum += val
         mittel = float(sum) / len(datastore["mcp_smoke_voltage"])
         smokeLevel = (22000*3.3)/(float(mittel)/1023*3.3) - 22000
-        form.lblSmokeLevel.setText("TGS2600: " + str(round(smokeLevel, 2)) + " Rs")
+        if form.mode_temp % 2 == 0:
+            form.lblSmokeLevel.setText("TGS2600: " + str(round(smokeLevel, 2)).replace(".", ",") + " Rs")
+        else:
+            form.lblSmokeLevel.setText("TGS2600: " + str(round(smokeLevel)) + " Rs")
 
-        form.lblTime.setText(datetime.now().strftime("%H:%M"))
+        format = "%H:%M"
+        if form.mode_clock == 1:
+            format = "%I:%M%p"
+        form.lblTime.setText(datetime.now().strftime(format))
 
 def main():
     app = QApplication(sys.argv)
