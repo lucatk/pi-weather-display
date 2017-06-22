b1447a78697c4dac93ba9375844511b4fc2a2dc4
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-22 14:23:42 +0200

    Fenster im Vollbild anzeigen

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from PyQt5.QtWidgets import *
 import mainwindow_auto
 
 class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
-  def __init__(self):
-    super(self.__class__, self).__init__()
-    self.setupUi(self)
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        self.setWindowFlags(PyQt5.QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(PyQt5.QtCore.Qt.FramelessWindowHint)
 
 def main():
-  app = QApplication(sys.argv)
-  form = MainWindow()
-  form.show()
-  sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    form = MainWindow()
+    form.show()
+    qdw = PyQt5.QtGui.QDesktopWidget()
+    screen = qdw.screenGeometry(screen=1)
+    form.centralWidget.setGeometry(screen)
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
   main()
